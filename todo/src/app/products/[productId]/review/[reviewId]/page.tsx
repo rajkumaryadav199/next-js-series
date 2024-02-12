733532670848f52ae99c33189326596ac905ca03
Rajkumar <kuraj10101999@gmail.com>
2024-02-13 00:21:27 +0530

    private folder done

--- todo/src/app/products/[productId]/review/[reviewId]/page.tsx
+++ todo/src/app/products/[productId]/review/[reviewId]/page.tsx
@@ -1,6 +1,11 @@
+import { notFound } from 'next/navigation'
 import React from 'react'
 
 const Review = ({params}:{params:{productId:string, reviewId:string}}) => {
+  if(parseInt(params.reviewId)>1001){
+    notFound()
+  }
+  console.log("parseInt", parseInt)
   return (
     <div>
       <h1>Reviw {params.reviewId} for products {params.productId}</h1>
