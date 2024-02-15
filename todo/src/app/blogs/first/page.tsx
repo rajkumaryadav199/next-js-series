552f8c1adef2493213663d4381c5d8516fc526e5
Rajkumar <kuraj10101999@gmail.com>
2024-02-16 00:12:59 +0530

    added route metadeta

--- todo/src/app/blogs/first/page.tsx
+++ todo/src/app/blogs/first/page.tsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import { Metadata } from "next"
+
+export const metadata:Metadata={
+  title:{
+    absolute:'Blog - mycode'
+  }
+}
 
 const FirstBlogs = () => {
   return (
